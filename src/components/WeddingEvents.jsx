608493bcf49205ad6fdab897a15ab66d7e9a3678
Harsha2717 <ncsriharsha90@gmail.com>
2026-03-22 10:57:28 +0530

    feat : removed reception element and updated String constants.

--- src/components/WeddingEvents.jsx
+++ src/components/WeddingEvents.jsx
@@ -5,13 +5,11 @@ import DisplayMap from "./GoogleMap";
 const wedding = "Wedding Ceremony";
 const haldi = "Haldi Ceremony";
 const mehendi = "Mehendi Celebration";
-const reception = "Reception";
 
 const events = [
-  { title: haldi, date: "12 December 2026", time: "10:00 AM", venue: "Venue Name, Hyderabad", icon: "🌸", lat: 17.542881, lng: 78.481445, zoom: 16 },
-  { title: mehendi, date: "12 December 2026", time: "5:00 PM", venue: "Venue Name, Hyderabad", icon: "🍃", lat: 17.542881, lng: 78.481445, zoom: 16 },
-  { title: wedding, date: "13 December 2026", time: "11:00 AM", venue: "The Grand Imperial, Jubilee Hills, Hyderabad", icon: "💍", lat: 17.4326, lng: 78.4071, zoom: 17 },
-  { title: reception, date: "13 December 2026", time: "7:00 PM", venue: "Venue Name, Hyderabad", icon: "✨", lat: 17.542881, lng: 78.481445, zoom: 16 }
+  { title: haldi, date: STRINGS.HALDI_DATE_STRING, time: STRINGS.HALDI_TIME_STRING, venue: STRINGS.HALDI_LOCATION, icon: "🌸", lat: 17.542881, lng: 78.481445, zoom: 16 },
+  { title: mehendi, date: STRINGS.MEHENDI_DATE_STRING, time: STRINGS.MEHENDI_TIME_STRING, venue: STRINGS.MEHENDI_LOCATION, icon: "🍃", lat: 17.542881, lng: 78.481445, zoom: 16 },
+  { title: wedding, date: STRINGS.WEDDING_DATE_STRING, time: STRINGS.WEDDING_TIME, venue: `${STRINGS.WEDDING_HALL}, ${STRINGS.WEDDING_LOCATION}`, icon: "💍", lat: STRINGS.WEDDING_HALL_LAT, lng: STRINGS.WEDDING_HALL_LNG, zoom: 17 },
 ];
 
 function WeddingEvents() {
